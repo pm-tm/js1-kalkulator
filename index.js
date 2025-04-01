6b42e3d3353d97a7b029c12c1a236afe5470f7c3
Piotr Muchowski TM <muchowski@menedzer.pl>
2025-04-01 12:03:22 +0200

    kalkulator

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('Happy developing ✨')
+let display = document.getElementById('display');
+
+function appendNumber(number) {
+    display.value += number;
+}
+
+function appendOperator(operator) {
+    display.value += operator;
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (e) {
+        display.value = 'Error';
+    }
+}
+
+function clearDisplay() {
+    display.value = '';
+}
